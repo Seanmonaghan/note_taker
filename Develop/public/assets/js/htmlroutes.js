844b899cd93db3e4f7bdb46c8cb5b7c6f07f0c47
Sean Monaghan <smonagha@conncoll.edu>
2021-04-05 16:53:41 -0400

    sorted out the pathing for css and js files

--- Develop/public/assets/js/htmlroutes.js
+++ Develop/public/assets/js/htmlroutes.js
@@ -1,12 +1,19 @@
 const path = require('path');
 
-
 module.exports = (app) => {
     
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../../notes.html'));
     });
-    
+
+    app.get('/assets/css/styles.css', (req, res) => {
+        res.sendFile(path.join(__dirname, '../css/styles.css'));
+    })
+
+    app.get('/assets/js/index.js', (req, res) => {
+        res.sendFile(path.join(__dirname, '../js/index.js'));
+    })
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../../index.html'));
     });
--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
-
 
 const app = express();
+
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-require('./htmlRoute')(app);
+require('./htmlroutes')(app);
+// require('./apiroutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`)
